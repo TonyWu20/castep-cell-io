1c9690d953db6fc799f4b3d844dc9509559cb2ca
TonyWu20 <tony.w21@gmail.com>
2024-04-28 00:45:45 +0800

    Change: Remove field `spin`; Add `Copy`, `Clone` derives; Implement `new` for `Mixture`

--- src/data/ionic_positions/mod.rs
+++ src/data/ionic_positions/mod.rs
@@ -1,27 +1,26 @@
 use castep_periodic_table::element::ElementSymbol;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct IonicPosition {
     symbol: ElementSymbol,
     coordinate: [f64; 3],
-    spin: Option<u8>,
     mixture: Option<Mixture>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub struct Mixture(usize, f64);
 
+impl Mixture {
+    pub fn new(id: usize, val: f64) -> Self {
+        Mixture(id, val)
+    }
+}
+
 impl IonicPosition {
-    pub fn new(
-        symbol: ElementSymbol,
-        coordinate: [f64; 3],
-        spin: Option<u8>,
-        mixture: Option<Mixture>,
-    ) -> Self {
+    pub fn new(symbol: ElementSymbol, coordinate: [f64; 3], mixture: Option<Mixture>) -> Self {
         Self {
             symbol,
             coordinate,
-            spin,
             mixture,
         }
     }
