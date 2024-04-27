1714e817d52e7935db862623687c920842fe5cf3
TonyWu20 <tony.w21@gmail.com>
2024-04-28 00:45:05 +0800

    Feat: parse lattice cell parameters and ionic positions

--- src/parsing/helpers/keywords/ionic_positions.rs
+++ src/parsing/helpers/keywords/ionic_positions.rs
@@ -1,19 +1,102 @@
-use winnow::{ascii::Caseless, combinator::alt, PResult, Parser};
+use std::str::FromStr;
 
-use crate::keywords::{DocumentSections, PositionsKeywords};
+use castep_periodic_table::element::ElementSymbol;
+use winnow::error::{ErrMode, InputError};
+use winnow::stream::AsChar;
+use winnow::{
+    ascii::{alpha1, digit1, float, space0, space1, Caseless},
+    combinator::{alt, preceded},
+    error::{ContextError, StrContext},
+    token::take_till,
+    PResult, Parser,
+};
 
-fn assign_positions_frac<'s>(input: &mut &'s str) -> PResult<DocumentSections> {
+use crate::parsing::helpers::block::get_block_data;
+use crate::{
+    data::{IonicPosition, Mixture},
+    keywords::{DocumentSections, PositionsKeywords},
+    parsing::CellParseError,
+};
+
+fn assign_positions_frac(input: &mut &str) -> PResult<DocumentSections> {
     Caseless("positions_frac")
         .map(|_| DocumentSections::IonicPositions(PositionsKeywords::POSITIONS_FRAC))
         .parse_next(input)
 }
 
-fn assign_positions_abs<'s>(input: &mut &'s str) -> PResult<DocumentSections> {
+fn assign_positions_abs(input: &mut &str) -> PResult<DocumentSections> {
     Caseless("positions_abs")
         .map(|_| DocumentSections::IonicPositions(PositionsKeywords::POSITIONS_ABS))
         .parse_next(input)
 }
 
-pub fn assign_positions_type<'s>(input: &mut &'s str) -> PResult<DocumentSections> {
+fn parse_mixture(input: &mut &str) -> PResult<Mixture> {
+    preceded(space1, Caseless("mixture")).parse_next(input)?;
+    take_till(0.., AsChar::is_dec_digit).parse_next(input)?;
+    if let Ok((id, _, val)) =
+        (digit1, space1, float::<&str, f64, InputError<&str>>).parse_next(input)
+    {
+        let id = id
+            .parse::<usize>()
+            .map_err(|_| ErrMode::Cut(ContextError::<StrContext>::new()))?;
+        return Ok(Mixture::new(id, val));
+    } else {
+        Err(ErrMode::Cut(ContextError::<StrContext>::new()))
+    }
+}
+
+pub fn assign_positions_type(input: &mut &str) -> PResult<DocumentSections> {
     alt((assign_positions_frac, assign_positions_abs)).parse_next(input)
+}
+
+fn parse_line_of_position(input: &mut &str) -> PResult<IonicPosition> {
+    let symbol = preceded(space0, alt((alpha1, digit1)))
+        .map(|s| {
+            ElementSymbol::from_str(s).map_err(|_| ErrMode::Cut(ContextError::<StrContext>::new()))
+        })
+        .parse_next(input)?;
+    let mut coord: Vec<f64> = Vec::new();
+    while let Ok(val) = preceded(space1, float::<&str, f64, InputError<&str>>).parse_next(input) {
+        coord.push(val)
+    }
+    let coord: [f64; 3] = coord
+        .try_into()
+        .map_err(|_| ErrMode::Cut(ContextError::<StrContext>::new()))?;
+    let mixture = parse_mixture(input).ok();
+    let position = IonicPosition::new(symbol?, coord, mixture);
+    Ok(position)
+}
+
+pub fn parse_ionic_positions(input: &mut &str) -> Result<Vec<IonicPosition>, CellParseError> {
+    let data = get_block_data(input).map_err(|_| CellParseError::GetBlockDataFailure)?;
+    let mut lines: Vec<&str> = data.lines().into_iter().collect();
+    lines
+        .iter_mut()
+        .map(|line| parse_line_of_position(line).map_err(|_| CellParseError::Invalid))
+        .collect()
+}
+
+#[cfg(test)]
+mod test {
+
+    use crate::parsing::helpers::{
+        block::get_block_data, keywords::ionic_positions::parse_ionic_positions,
+    };
+
+    #[test]
+    fn keywords_position() {
+        let mut input = "  C  0.0756034347004260  0.0756034355668187  0.5000000004346841
+  C  0.1496332166229109  0.1496332194727908  0.5000000000710555
+  C  0.2145289813410105  0.2145289823390266  0.4999999994942101
+  C  0.4243965644332829 -0.0000000008663758  0.5000000004346841
+  C  0.3503667805273500 -0.0000000028498315  0.5000000000710555
+  V  0.3934837276229430  0.6065302751523840  0.5001896946183580 SPIN=  2.0000000000
+%ENDBLOCK POSITIONS_FRAC
+
+";
+        let mut data = get_block_data(&mut input).unwrap();
+        println!("{:#}", data);
+        let positions = parse_ionic_positions(&mut data);
+        println!("{:#?}", positions);
+    }
 }
--- src/parsing/helpers/keywords/lattice.rs
+++ src/parsing/helpers/keywords/lattice.rs
@@ -1,19 +1,99 @@
 use winnow::{ascii::Caseless, combinator::alt, PResult, Parser};
 
-use crate::keywords::{CellLatticeVectorsKeywords, DocumentSections};
+use crate::{
+    data::{LatticeABC, LatticeCart, LatticeParam},
+    keywords::{DocumentSections, LatticeBlockType},
+    parsing::{helpers::block::get_block_data, CellParseError},
+};
 
-fn assign_lattice_cart<'s>(input: &mut &'s str) -> PResult<DocumentSections> {
+fn assign_lattice_cart(input: &mut &str) -> PResult<DocumentSections> {
     Caseless("lattice_cart")
-        .map(|_| DocumentSections::CellLatticeVectors(CellLatticeVectorsKeywords::LATTICE_CART))
+        .map(|_| DocumentSections::CellLatticeVectors(LatticeBlockType::LATTICE_CART))
         .parse_next(input)
 }
 
-fn assign_lattice_abc<'s>(input: &mut &'s str) -> PResult<DocumentSections> {
+fn assign_lattice_abc(input: &mut &str) -> PResult<DocumentSections> {
     Caseless("lattice_abc")
-        .map(|_| DocumentSections::CellLatticeVectors(CellLatticeVectorsKeywords::LATTICE_ABC))
+        .map(|_| DocumentSections::CellLatticeVectors(LatticeBlockType::LATTICE_ABC))
         .parse_next(input)
 }
 
-pub fn assign_lattice_type<'s>(input: &mut &'s str) -> PResult<DocumentSections> {
+pub fn assign_lattice_type(input: &mut &str) -> PResult<DocumentSections> {
     alt((assign_lattice_abc, assign_lattice_cart)).parse_next(input)
+}
+
+/// This should be receiving the output of `get_block_data()`
+fn parse_lattice_cart(data_input: &str) -> Result<LatticeParam, CellParseError> {
+    let values: Vec<f64> = data_input
+        .split_whitespace()
+        .filter_map(|s| s.parse::<f64>().ok())
+        .collect();
+    if values.len() != 9 {
+        return Err(CellParseError::UnexpectedLength);
+    }
+    let array: [f64; 9] = values.try_into().unwrap();
+    let a: [f64; 3] = array[0..3].try_into().unwrap();
+    let b: [f64; 3] = array[3..6].try_into().unwrap();
+    let c: [f64; 3] = array[6..9].try_into().unwrap();
+    let lattice_cart = LatticeCart::new(a, b, c);
+    Ok(LatticeParam::LatticeCart(lattice_cart))
+}
+
+/// This should be receiving the output of `get_block_data()`
+fn parse_lattice_abc(data_input: &str) -> Result<LatticeParam, CellParseError> {
+    let values: Vec<f64> = data_input
+        .split_whitespace()
+        .filter_map(|s| s.parse::<f64>().ok())
+        .collect();
+    if values.len() != 6 {
+        return Err(CellParseError::UnexpectedLength);
+    }
+    let a = values[0];
+    let b = values[1];
+    let c = values[2];
+    let alpha = values[3];
+    let beta = values[4];
+    let gamma = values[5];
+    let lattice_abc = LatticeABC::new(a, b, c, alpha, beta, gamma);
+    Ok(LatticeParam::LatticeABC(lattice_abc))
+}
+
+pub fn parse_lattice_param(
+    input: &mut &str,
+    lat_type: LatticeBlockType,
+) -> Result<LatticeParam, CellParseError> {
+    let data_input = get_block_data(input).map_err(|_| CellParseError::Invalid)?;
+    match lat_type {
+        LatticeBlockType::LATTICE_CART => parse_lattice_cart(data_input),
+        LatticeBlockType::LATTICE_ABC => parse_lattice_abc(data_input),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{
+        keywords::{DocumentSections, LatticeBlockType},
+        parsing::helpers::{block::get_block_data, current_sections},
+    };
+
+    use super::parse_lattice_cart;
+
+    #[test]
+    fn keywords_lattice() {
+        let mut input = "%BLOCK LATTICE_CART
+   18.931530020488704480   -0.000000000000003553    0.000000000000000000
+   -9.465765010246645517   16.395185930251127360    0.000000000000000000
+    0.000000000000000000    0.000000000000000000    9.999213039981000861
+%ENDBLOCK LATTICE_CART
+";
+        let section = current_sections(&mut input).unwrap();
+        match section {
+            DocumentSections::CellLatticeVectors(LatticeBlockType::LATTICE_CART) => {
+                let data = get_block_data(&mut input).unwrap();
+                let lattice_cart = parse_lattice_cart(data).unwrap();
+                println!("{:?}", lattice_cart);
+            }
+            _ => (),
+        }
+    }
 }
