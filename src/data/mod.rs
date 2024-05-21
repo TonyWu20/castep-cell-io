e1a0260d81f48aa8033b037fda523ecaebf09f6a
TonyWu20 <tony.w21@gmail.com>
2024-05-21 11:28:28 +0800

    Major: `write_out` of `CellDocument` now perform `write` directly

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -2,6 +2,8 @@ mod ionic_positions;
 mod lattice_parameters;
 mod units;
 
+use std::{fs, io::Error, path::Path};
+
 #[allow(unused_imports)]
 pub use ionic_positions::{IonicPosition, IonicPositionBlock, Mixture};
 #[allow(unused_imports)]
@@ -31,12 +33,17 @@ impl CellDocument {
     pub fn ionic_positions(&self) -> &IonicPositionBlock {
         &self.ionic_positions
     }
-    pub fn write_out(&self) -> String {
-        [
+    pub fn write_out<P: AsRef<Path>>(&self, path: P) -> Result<(), Error> {
+        let contents = [
             format!("{}", self.lattice),
             format!("{}", self.ionic_positions),
         ]
-        .concat()
+        .concat();
+        fs::write(path, contents)
+    }
+
+    pub fn ionic_positions_mut(&mut self) -> &mut IonicPositionBlock {
+        &mut self.ionic_positions
     }
 }
 
