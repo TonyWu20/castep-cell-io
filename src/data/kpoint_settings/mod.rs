0134646c238db64ad7ec6fd9457732cd12bc6d76
TonyWu20 <tony.w21@gmail.com>
2024-06-14 12:55:35 +0800

    `CellEntries` to hold other possible sections in a `CellDocument`

--- src/data/kpoint_settings/mod.rs
+++ src/data/kpoint_settings/mod.rs
@@ -1,13 +1,53 @@
+use std::fmt::Display;
+
 use crate::InvLengthUnit;
 
 pub mod mp_settings;
+pub mod nc_settings;
 
 #[derive(Debug, Clone, Copy, Default)]
 pub enum KpointTask {
     #[default]
+    #[allow(clippy::upper_case_acronyms)]
     SCF,
     Spectral,
     Phonon,
+}
+
+#[derive(Debug, Clone)]
+pub enum KpointSettings {
+    List(KpointListBlock),
+    MPGrid(KpointMPGrid),
+    MPSpacing(KpointMPSpacing),
+}
+
+impl Display for KpointSettings {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let content = match self {
+            KpointSettings::List(list_block) => format!("{}", list_block),
+            KpointSettings::MPGrid(grid) => format!("{}", grid),
+            KpointSettings::MPSpacing(spacing) => format!("{}", spacing),
+        };
+        write!(f, "{content}")
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum NCKpointSettings {
+    List(KpointListBlock),
+    Path(BSKpointPath),
+    PathSpacing(BSKpointPathSpacing),
+}
+
+impl Display for NCKpointSettings {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let content = match self {
+            NCKpointSettings::List(l) => format!("{}", l),
+            NCKpointSettings::Path(pth) => format!("{}", pth),
+            NCKpointSettings::PathSpacing(pth_spacing) => format!("{}", pth_spacing),
+        };
+        write!(f, "{content}")
+    }
 }
 
 /// The first three entries on a line are the fractional positions of the k-point
@@ -23,6 +63,13 @@ pub struct KpointListBlock {
     kpoint_list: Vec<[f64; 4]>,
 }
 
+impl KpointListBlock {
+    pub fn new(task: KpointTask, kpoint_list: Vec<[f64; 4]>) -> Self {
+        Self { task, kpoint_list }
+    }
+}
+
+/// # Example
 /// `KPOINT_MP_GRID I_i I_j I_k`
 /// This specifies the dimensions of the Monkhorst-Pack grid requested in the
 /// directions of the reciprocal space lattice vectors. The generated grid will
@@ -33,6 +80,13 @@ pub struct KpointMPGrid {
     grid: [u32; 3],
 }
 
+impl KpointMPGrid {
+    pub fn new(task: KpointTask, grid: [u32; 3]) -> Self {
+        Self { task, grid }
+    }
+}
+
+/// # Example
 /// `KPOINT_MP_SPACING R [units]`
 /// The single entry is the maximum distance between k-points on the Monkhorst-Pack
 /// grid. The dimensions of the grid will be chosen such that the maximum separation
@@ -44,8 +98,54 @@ pub struct KpointMPSpacing {
     unit: InvLengthUnit,
 }
 
+impl KpointMPSpacing {
+    pub fn new(task: KpointTask, spacing: f64, unit: InvLengthUnit) -> Self {
+        Self {
+            task,
+            spacing,
+            unit,
+        }
+    }
+}
+
+/// # Example
 /// `KPOINT_MP_OFFSET R_i R_j R_k`
 /// This specifies the offset of the Monkhorst-Pack grid with respect to the
 /// origin of the Brillouin zone. The three entries are the offset in fractional
 /// coordinates relative to the reciprocal lattice vectors.
-pub struct KpointMPOffset([f64; 3]);
+#[derive(Debug, Clone, Copy)]
+pub struct KpointMPOffset {
+    offset: [f64; 3],
+}
+
+impl KpointMPOffset {
+    pub fn new(offset: [f64; 3]) -> Self {
+        Self { offset }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct BSKpointPath {
+    paths: Vec<[f64; 3]>,
+}
+
+impl BSKpointPath {
+    pub fn new(paths: Vec<[f64; 3]>) -> Self {
+        Self { paths }
+    }
+}
+
+/// # Example
+/// BS_KPOINT_PATH_SPACING : 0.125
+/// This keyword determines the maximum spacing between k-points used for non-self consistent band structure calculations, along the path specified by `BS_KPOINT_PATH`.
+#[derive(Debug, Clone)]
+pub struct BSKpointPathSpacing {
+    unit: InvLengthUnit,
+    spacing: f64,
+}
+
+impl BSKpointPathSpacing {
+    pub fn new(unit: InvLengthUnit, spacing: f64) -> Self {
+        Self { unit, spacing }
+    }
+}
--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,21 +1,28 @@
+#[allow(unused_imports)]
+mod constraints;
 mod ionic_positions;
 mod kpoint_settings;
 mod lattice_parameters;
+mod species_characters;
 pub mod units;
 
-use std::{fs, io::Error, path::Path};
+use std::{fmt::Display, fs, io::Error, path::Path};
 
-#[allow(unused_imports)]
 pub use ionic_positions::{IonicPosition, IonicPositionBlock, Mixture};
-#[allow(unused_imports)]
+pub use kpoint_settings::*;
 pub use lattice_parameters::{LatticeABC, LatticeCart, LatticeParam, LatticeParamBlock};
-#[allow(unused_imports)]
+pub use species_characters::{
+    LCAOBasis, PseudoPot, SpeciesLCAOStatesBlock, SpeciesMass, SpeciesMassBlock, SpeciesPotBlock,
+};
+
+pub use constraints::{FixAllCell, FixAllIons, FixCom};
 
 /// A structure to represent the `.cell` file.
 #[derive(Debug, Clone)]
 pub struct CellDocument {
     lattice: LatticeParamBlock,
     ionic_positions: IonicPositionBlock,
+    entries: Option<Vec<CellEntries>>,
 }
 
 impl CellDocument {
@@ -23,6 +30,7 @@ impl CellDocument {
         Self {
             lattice,
             ionic_positions,
+            entries: None,
         }
     }
 
@@ -40,16 +48,73 @@ impl CellDocument {
     pub fn ionic_positions_mut(&mut self) -> &mut IonicPositionBlock {
         &mut self.ionic_positions
     }
-}
 
-impl ToString for CellDocument {
-    fn to_string(&self) -> String {
-        [
-            format!("{}", self.lattice),
-            format!("{}", self.ionic_positions),
-        ]
-        .concat()
+    pub fn entries(&self) -> Option<&Vec<CellEntries>> {
+        self.entries.as_ref()
+    }
+
+    pub fn entries_mut(&mut self) -> &mut Option<Vec<CellEntries>> {
+        &mut self.entries
+    }
+
+    pub fn set_entries(&mut self, entries: Option<Vec<CellEntries>>) {
+        self.entries = entries;
     }
 }
 
-pub trait CellData {}
+impl Display for CellDocument {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let entries = self
+            .entries()
+            .map(|e| {
+                e.iter()
+                    .map(|item| format!("{}", item))
+                    .collect::<Vec<String>>()
+                    .join("\n")
+            })
+            .unwrap_or_default();
+        let content = [
+            format!("{}", self.lattice),
+            format!("{}", self.ionic_positions),
+            entries,
+        ]
+        .concat();
+        write!(f, "{}", content)
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum CellEntries {
+    KpointSettings(KpointSettings),
+    NCKpointSettings(NCKpointSettings),
+    /// This keyword controls whether or not all of the lattice parameters remain fixed during relaxation or molecular dynamics.
+    FixAllCell(FixAllCell),
+    FixAllIons(FixAllIons),
+    /// This keyword controls whether or not the center of mass of the ions remains fixed during relaxation or molecular dynamics.
+    FixCom(FixCom),
+    IonicConstraints,
+    ExtEfield,
+    ExtPressure,
+    SpeciesMass(SpeciesMassBlock),
+    SpeciesPot,
+    SpeciesLCAOStates,
+}
+
+impl Display for CellEntries {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let content = match self {
+            CellEntries::KpointSettings(v) => format!("{v}"),
+            CellEntries::NCKpointSettings(v) => format!("{v}"),
+            CellEntries::FixAllCell(v) => format!("{v}"),
+            CellEntries::FixAllIons(v) => format!("{v}"),
+            CellEntries::FixCom(v) => format!("{v}"),
+            CellEntries::IonicConstraints => todo!(),
+            CellEntries::ExtEfield => todo!(),
+            CellEntries::ExtPressure => todo!(),
+            CellEntries::SpeciesMass(v) => format!("{v}"),
+            CellEntries::SpeciesPot => todo!(),
+            CellEntries::SpeciesLCAOStates => todo!(),
+        };
+        write!(f, "{content}")
+    }
+}
