8394ce9ef275137177524dc63f51866f82d6685d
TonyWu20 <tony.w21@gmail.com>
2024-06-14 12:56:09 +0800

    Avoid confusion of `to_string`

--- src/formatting/mod.rs
+++ src/formatting/mod.rs
@@ -1,6 +1,4 @@
-
 pub trait BlockDisplay {
-
     fn block_tag(&self) -> String;
     fn entries(&self) -> String;
     fn content(&self) -> String {
@@ -17,7 +15,7 @@ pub trait BlockDisplay {
 pub trait FieldDisplay {
     fn field_tag(&self) -> String;
     fn value(&self) -> String;
-    fn to_string(&self) -> String {
+    fn content(&self) -> String {
         format!("{} : {}", self.field_tag(), self.value())
     }
 }
