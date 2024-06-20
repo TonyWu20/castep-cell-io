9f35e3995df8757422d89ce875a0854a5cc104a1
TonyWu20 <tony.w21@gmail.com>
2024-06-20 11:24:32 +0800

    Pub re-export `CastepParams` and `CastepParamsBuilder`

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,10 @@ mod parsing;
 
 pub use calc_setup::SeedfileGenerator;
 pub use cell_document::{
-    params::CastepTask, units::length_units::*, CellDocument, IonicPosition, IonicPositionBlock,
-    LatticeABC, LatticeCart, LatticeParam, LatticeParamBlock,
+    params::{CastepParams, CastepParamsBuilder, CastepTask},
+    units::length_units::*,
+    CellDocument, IonicPosition, IonicPositionBlock, LatticeABC, LatticeCart, LatticeParam,
+    LatticeParamBlock,
 };
 pub use keywords::{DocumentSections, KPointKeywords, LatticeBlockType, PositionsKeywords};
 pub use parsing::{CellParseError, CellParser};
