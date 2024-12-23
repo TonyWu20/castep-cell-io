c25e7fd061e3ecc6eb74e679096c57a49533a959
TonyWu20 <tony.w21@gmail.com>
2024-12-23 22:01:28 +0800

    Pub export module `cell_document`

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,14 @@
 #![allow(dead_code)]
 
-mod calc_setup;
-mod cell_document;
+pub mod cell_document;
 mod formatting;
 mod keywords;
 mod parsing;
 
-pub use calc_setup::{CellBuilding, ParamBuilding};
 pub use cell_document::{
     params::{CastepParams, CastepParamsBuilder, CastepTask, EnergyCutoff, EnergyCutoffError},
     to_cell_document,
     units::length_units::*,
-    CellDocument, IonicPosition, IonicPositionBlock, KpointQuality, LatticeABC, LatticeCart,
-    LatticeParam, LatticeParamBlock,
 };
 pub use keywords::{
     DocumentSections, KPointKeywords, KeywordType, LatticeBlockType, PositionsKeywords,
