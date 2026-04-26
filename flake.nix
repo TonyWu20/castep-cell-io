fbbba06fb44dfb7c9b9085e858d73df30e9e7a3f
TonyWu20 <tony.w21@gmail.com>
2026-04-26 19:41:34 +0800

    Update flake

--- flake.nix
+++ flake.nix
@@ -2,19 +2,16 @@
   description = "rust environment";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    naersk = {
-      url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    devshell.url = "github:numtide/devshell";
   };
-  outputs = { nixpkgs, fenix, naersk, ... }:
+  outputs = { nixpkgs, fenix, devshell, ... }:
     let
       systems = [ "x86_64-linux" "aarch64-darwin" ];
-      pkgsFor = system: import nixpkgs { inherit system; overlays = [ fenix.overlays.default ]; };
+      pkgsFor = system: import nixpkgs { inherit system; overlays = [ fenix.overlays.default devshell.overlays.default ]; };
 
       forAllSystems = nixpkgs.lib.genAttrs systems;
     in
@@ -25,33 +22,43 @@
           pkgs = pkgsFor system;
         in
         {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              (fenix.packages.${system}.stable.withComponents [
-                "cargo"
-                "clippy"
-                "rust-src"
-                "rustc"
-                "rustfmt"
-                "rust-analyzer"
-              ])
-              stdenv
-              fish
-              (python313.withPackages (ps: with ps; [
-                beautifulsoup4
-                requests
-                pynvim
-              ]))
-              uv
-            ];
-            env = {
-              CLAUDE_CODE_NO_FLICKER = 1;
+          default = pkgs.devshell.mkShell
+            {
+              packages = with pkgs; [
+                (fenix.packages.${system}.stable.withComponents [
+                  "cargo"
+                  "clippy"
+                  "rust-src"
+                  "rustc"
+                  "rustfmt"
+                  "rust-analyzer"
+                ])
+                stdenv
+                fish
+                (python313.withPackages (ps: with ps; [
+                  beautifulsoup4
+                  requests
+                  pynvim
+                ]))
+                uv
+              ];
+              commands = [
+                {
+                  name = "claude-deepseek";
+                  command = ''
+                    ANTHROPIC_BASE_URL=$DEEPSEEK_BASE_URL \
+                    ANTHROPIC_AUTH_TOKEN=$DEEPSEEK_TOKEN \
+                    CLAUDE_CODE_ATTRIBUTION_HEADER="0" \
+                    CLAUDE_CODE_EFFORT_LEVEL=max \
+                    ANTHROPIC_DEFAULT_OPUS_MODEL=deepseek-v4-pro \
+                    ANTHROPIC_DEFAULT_SONNET_MODEL=deepseek-v4-flash \
+                    ANTHROPIC_DEFAULT_HAIKU_MODEL=deepseek-v4-flash \
+                    claude --model "opusplan [1m]"
+                  '';
+                }
+              ];
             };
-          };
         }
       );
-
-
     };
-
 }
