457723556fedfba05873ce4c607b86db80549f89
TonyWu20 <tony.w21@gmail.com>
2026-05-04 02:37:32 +0800

    feat(issue-8): TASK-G3-2 (TDD): Create SymmetryGenerate unit struct flag type

--- castep_cell_io/src/cell/symmetry/mod.rs
+++ castep_cell_io/src/cell/symmetry/mod.rs
@@ -1,5 +1,7 @@
 mod symmetry_ops;
 mod symmetry_tol;
+mod symmetry_generate;
 
 pub use symmetry_ops::{SymmetryOp, SymmetryOps};
 pub use symmetry_tol::SymmetryTol;
+pub use symmetry_generate::SymmetryGenerate;
